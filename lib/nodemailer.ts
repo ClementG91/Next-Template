b82c7f02a05018730c5b9321b323c104a1ba8ec7
ClementG91 <clement.genest91@gmail.com>
2024-10-20 14:57:51 +0200

    Simplify nodemailer configuration for better flexibility

--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -1,7 +1,8 @@
 import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: process.env.EMAIL_SERVER_HOST,
+  port: Number(process.env.EMAIL_SERVER_PORT),
   auth: {
     user: process.env.EMAIL_SERVER_USER,
     pass: process.env.EMAIL_SERVER_PASSWORD,
